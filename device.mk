ee6aaa8a0cbc5bf87527101b34af789074dd834f
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-10 07:00:26 +0000

    sdm660-common: Uprev RIL interfaces

--- device.mk
+++ device.mk
@@ -409,7 +409,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio@1.5 \
     android.hardware.secure_element@1.2 \
     libavservices_minijail.vendor \
