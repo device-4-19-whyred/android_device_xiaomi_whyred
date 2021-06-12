d87f578a6678fc27e545a7dfd0db41be2b2239dc
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-12 15:47:25 +0000

    clenup

--- device.mk
+++ device.mk
@@ -450,10 +450,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
