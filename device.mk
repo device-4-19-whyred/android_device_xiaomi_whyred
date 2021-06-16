d25c6f5c47925042c972cd1364782dc8a7a79bf9
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-16 05:52:54 +0000

    whyred:Update Blobs from LA.UM.9.1-r1-06700-Smxxx0.0

--- device.mk
+++ device.mk
@@ -487,7 +487,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.mock
 
 # Thermal Conf
 PRODUCT_COPY_FILES += \
