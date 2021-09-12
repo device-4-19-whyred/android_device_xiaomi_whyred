fedcade0b4556879211949a6179c88d3158e1c1e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:44:17 +0200

    sdm660-common: libhidl: Move to Android.bp
    * Make the lib vendor_available and remove it from vndk-ext
    
    Change-Id: I3945a460d563aaf07aaa7b757bb9c0dddc4f91cd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,12 @@
 #rm -rf hardware/qcom-caf/msm8998/audio
 #rm -rf frameworks/base
 #rm -rf packages/apps/Settings
-#rm -rf frameworks/av#
+rm -rf frameworks/av
 #rm -rf kernel/xiaomi/whyred
 # git clone
 git clone https://github.com/11-whyred-new/hal_display_4.19 hardware/qcom-caf/msm8998/display
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/11-whyred-new/hal_audio_4.19 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/11-whyred-new/hal_media_4.19 hardware/qcom-caf/msm8998/media
 #git clone https://github.com/dotMod/android_frameworks_base -b dot11 frameworks/base
 #git clone https://github.com/dotMod/android_packages_apps_Settings -b dot11 packages/apps/Settings
 #git clone https://github.com/dotMod/android_frameworks_av-1 -b dot11 frameworks/av
