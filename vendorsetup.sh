e153c21112471b7e93dae355a54f6e11be5eb80f
predator112 <mi460334@gmail.com>
2021-09-17 04:34:25 +0000

    gut

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,15 @@
 # rm -rf 
 #rm -rf hardware/qcom-caf/msm8998/display
 #rm -rf hardware/qcom-caf/msm8998/media
-#rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/audio
 #rm -rf frameworks/base
 #rm -rf packages/apps/Settings
 #rm -rf frameworks/av
 #rm -rf kernel/xiaomi/whyred
 # git clone
-git clone https://github.com/11-whyred-new/hal_display_4.19 hardware/qcom-caf/msm8998/display
-git clone https://github.com/11-whyred-new/hal_audio_4.19 hardware/qcom-caf/msm8998/audio
-git clone https://github.com/11-whyred-new/hal_media_4.19 hardware/qcom-caf/msm8998/media
+git clone https://github.com/ParsimoniousLavender/hardware_qcom-caf_display_msm8998 -b eleven-4.19 hardware/qcom-caf/msm8998/display
+git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_audio -b audio-hal.lnx.8.0.r14-rel hardware/qcom-caf/msm8998/audio
+git clone https://github.com/ParsimoniousLavender/hardware_qcom-caf_media_msm8998 -b eleven-4.19 hardware/qcom-caf/msm8998/media
 #git clone https://github.com/dotMod/android_frameworks_base -b dot11 frameworks/base
 #git clone https://github.com/dotMod/android_packages_apps_Settings -b dot11 packages/apps/Settings
 git clone https://github.com/dotMod/android_frameworks_av-1 -b dot11 frameworks/av
