a6265f110e748fbddeaec45ffdaaaf848e0f944a
predator112 <mi460334@gmail.com>
2021-09-20 01:44:42 +0000

    add

--- device.mk
+++ device.mk
@@ -198,8 +198,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64 \
-    android.hardware.camera.common@1.0
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Component overrides
 PRODUCT_COPY_FILES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,11 @@
 # rm -rf 
-rm -rf hardware/qcom-caf/msm8998/display
-rm -rf hardware/qcom-caf/msm8998/media
-rm -rf hardware/qcom-caf/msm8998/audio
+#rm -rf hardware/qcom-caf/msm8998/display
+#rm -rf hardware/qcom-caf/msm8998/media
+#rm -rf hardware/qcom-caf/msm8998/audio
 #rm -rf frameworks/base
 #rm -rf packages/apps/Settings
 #rm -rf frameworks/av
+#rm -rf vendor/MiuiCamera
 #rm -rf kernel/xiaomi/whyred
 # git clone
 #git clone https://github.com/11-whyred-new/hal_display_4.19 hardware/qcom-caf/msm8998/display
@@ -13,7 +14,7 @@ rm -rf hardware/qcom-caf/msm8998/audio
 #git clone https://github.com/dotMod/android_frameworks_base -b dot11 frameworks/base
 #git clone https://github.com/dotMod/android_packages_apps_Settings -b dot11 packages/apps/Settings
 git clone https://github.com/dotMod/android_frameworks_av-1 -b dot11 frameworks/av
-#git clone https://github.com/11-whyred-new/MiuiCamera -b cr-9.0 vendor/MiuiCamera
+git clone https://github.com/11-whyred-new/MiuiCamera -b cr-9.0 vendor/MiuiCamera
 git clone https://github.com/predator112/whyred-4.19 -b lineage-18.1 kernel/xiaomi/whyred
 #rm -rf external/tinycompress
 git clone https://github.com/pkm774/external_tinycompress -b ks-aosp.lnx.3.0.r15-rel external/tinycompress
